f976d0f7f8b2a7397a9edc1052a3c41f3b0de581
imt-eiaz <imteiaz@gmail.com>
2025-08-11 22:32:39 +0100

    spread operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,25 +11,7 @@ function App() {
     const { value, name } = event.target;
 
     setContact((preValue) => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: preValue.lName,
-          email: preValue.email,
-        };
-      } else if (name === "lName") {
-        return {
-          fName: preValue.fName,
-          lName: value,
-          email: preValue.email,
-        };
-      } else if (name === "email") {
-        return {
-          fName: preValue.fName,
-          lName: preValue.lName,
-          email: value,
-        };
-      }
+      return { ...preValue, [name]: value };
     });
   }
 
